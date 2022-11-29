76755be7373561986d80e2d7c80051b0f2152608
sksingtn <shiv.k.singh@outlook.com>
2022-11-30 04:24:37 +0530

    Fixed an issue with asset not being bundled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import Todo from "./components/Todo/Todo"
 import styled from "styled-components";
+import mainLogo from'./assets/background.svg';
 import "./App.css";
 
 
 const MainContainer = styled.div`
 width: 100vw;
 height: 100vh;
-background:url("./src/assets/background.svg");
+background:url(${mainLogo});
 padding-top: 3em;
 display: flex;
 justify-content: center;
